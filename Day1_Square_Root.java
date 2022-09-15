f5900cb280a738f608760a054161d81da874f969
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-15 19:40:25 +0530

    Add files via upload

---
+++ Day1_Square_Root.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class Day1_Square_Root {
+    public static void main(String[] args) {
+        int number = 100;
+        double result = Math.sqrt(number);
+        System.out.println("Square root of "+number+" is "+result);
+    }
+}
+
+// Square root of 100 is 10.0
