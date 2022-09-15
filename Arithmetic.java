4eda645d8d9f709d68006ddf56e294de1deeb67c
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-15 14:51:45 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a=20, b=10;
+		System.out.println(a+" + "+b+" = "+(a+b));
+		System.out.println(a+" - "+b+" = "+(a-b));
+		System.out.println(a+" * "+b+" = "+(a*b));
+		System.out.println(a+" / "+b+" = "+(a/b));
+
+	}
+
+}
