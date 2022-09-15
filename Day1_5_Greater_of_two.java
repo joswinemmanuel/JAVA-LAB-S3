d14f7f6ea748975157cc95c6d5c9a57a59cfe1fb
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-15 19:57:05 +0530

    Add files via upload

---
+++ Day1_5_Greater_of_two.java
@@ -0,0 +1,9 @@
+public class Day1_5_Greater_of_two {
+    public static void main(String[] args) {
+        int a=100, b=200, greater;
+        greater = (a>b) ? a : b;
+        System.out.println("Greater of "+a+" and "+b+" is "+greater);
+    }
+}
+
+// Greater of 100 and 200 is 200
