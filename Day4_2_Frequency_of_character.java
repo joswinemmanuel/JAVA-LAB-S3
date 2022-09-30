b8062b1a4767a90b2f6fe15063e3ae457e743735
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-30 22:44:15 +0530

    Update Day4_2_Frequency_of_character.java

--- Day4_2_Frequency_of_character.java
+++ Day4_2_Frequency_of_character.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Day4_First_last_index {
+public class Day4_2_Frequency_of_character {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str;
