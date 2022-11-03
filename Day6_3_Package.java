41e8e3b4e27eaaeff4fef62254ea25e64929b521
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-11-03 15:22:41 +0530

    How to use package in java

--- Day6_3_Package.java
+++ Day6_3_Package.java
@@ -1,14 +1,39 @@
 /*
+// In MyPackage, create two classes FirstClass.java and SecondClass.java
+
+// In FirstClass.java
 
 package MyPackage;
+public class FirstClass {
+	public void printFirst() {
+		System.out.println("This is first class");
+	}
+}
 
-class FirstClass {};
-class SecondClass {};
+// In SecondClass.java
+
+package MyPackage;
+public class SecondClass {
+	public void printSecond() {
+		System.out.println("This is second class");
+	}
+}
+
+
+// In the default package, create class Demo.java
 
 // In Demo.java
 
-import MyPackage.FirstClass;
-        or
 import MyPackage.*;
+public class Demo {
+	public static void main(String[] args) {
 
+		FirstClass obj1 = new FirstClass();
+		obj1.printFirst();
+		
+		SecondClass obj2 = new SecondClass();
+		obj2.printSecond()
+		
+	}
+}
 */
