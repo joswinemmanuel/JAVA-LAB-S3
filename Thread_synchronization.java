17243a4cd2da68b50eac0ffa5daed769c1e64932
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-12-22 11:56:43 +0530

    Thread Synchronized

--- Thread_synchronization.java
+++ Thread_synchronization.java
@@ -1,108 +1,49 @@
-class PrintTest extends Thread
+class PrintTest extends Thread {
+	synchronized public void printThread(int n) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Thread " + n + " is working...");
 
-{
+			try {
+				Thread.sleep(600);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-synchronized public void printThread(int n)
+		}
 
-{
-
-for (int i = 1; i <= 5; i++)
-
-{
-
-System.out.println("Thread " + n + " is working...");
-
-try
-
-{
-
-Thread.sleep(600);
-
+		System.out.println("--------------------------");
+	}
 }
 
-catch (Exception e)
+class Thread1 extends Thread {
+	PrintTest test;
+	Thread1(PrintTest p) {
+		test = p;
+	}
 
-{
-
-e.printStackTrace();
-
+	public void run() {
+		test.printThread(1);
+	}
 }
 
+class Thread2 extends Thread {
+	PrintTest test;
+	Thread2(PrintTest p) {
+		test = p;
+	}
+
+	public void run() {
+		test.printThread(2);
+	}
 }
 
-System.out.println("--------------------------");
+public class thread {
+	public static void main(String[] args) {
+		PrintTest p = new PrintTest();
+		Thread1 t1 = new Thread1(p);
+		Thread2 t2 = new Thread2(p);
 
+		t1.start();
+		t2.start();
+	}
 }
-
-}
-
-class Thread1 extends Thread
-
-{
-
-PrintTest test;
-
-Thread1(PrintTest p)
-
-{
-
-test = p;
-
-}
-
-public void run()
-
-{
-
-test.printThread(1);
-
-}
-
-}
-
-class Thread2 extends Thread
-
-{
-
-PrintTest test;
-
-Thread2(PrintTest p)
-
-{
-
-test = p;
-
-}
-
-public void run()
-
-{
-
-test.printThread(2);
-
-}
-
-}
-
-public class ThreadSync
-
-{
-
-public static void main(String[] args)
-
-{
-
-PrintTest p = new PrintTest();
-
-Thread1 t1 = new Thread1(p);
-
-Thread2 t2 = new Thread2(p);
-
-t1.start();
-
-t2.start();
-
-}
-
-}
-
