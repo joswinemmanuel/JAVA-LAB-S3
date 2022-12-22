596e25f8eed36a475e6c9b6fde8c5e12047e9f99
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-12-22 11:09:17 +0530

    Complete Calculator dummy

--- Calculator_interface.java
+++ Calculator_interface.java
@@ -3,13 +3,13 @@ import javax.swing.*;
 
 
 public class GUI_Calculator implements ActionListener {
+	JTextField t1;
+	JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16;
+	int n,i,flag;
 	
 	GUI_Calculator() {
 		JFrame f = new JFrame("CALCULATOR");
 		JPanel p = new JPanel();
-		
-		JTextField t1;
-		JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16;
 		
 		t1 = new JTextField();
 		t1.setBounds(100, 100, 200, 30);
@@ -46,6 +46,23 @@ public class GUI_Calculator implements ActionListener {
 		b16 = new JButton("C");
 		b16.setBounds(250, 230, 50, 30);
 		
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		b8.addActionListener(this);
+		b9.addActionListener(this);
+		b10.addActionListener(this);
+		b11.addActionListener(this);
+		b12.addActionListener(this);
+		b13.addActionListener(this);
+		b14.addActionListener(this);
+		b15.addActionListener(this);
+		b16.addActionListener(this);
+		
 		p.add(t1);
 		p.add(b1);
 		p.add(b2);
@@ -69,6 +86,87 @@ public class GUI_Calculator implements ActionListener {
 		f.setSize(400,500);
 		f.setLayout(null);
 		f.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+	     if(e.getSource() == b1)
+	         t1.setText(t1.getText() + "1");
+	     else if(e.getSource() == b2)
+	         t1.setText(t1.getText() + "2");
+	     else if(e.getSource() == b3)
+	         t1.setText(t1.getText() + "3");
+	     else if(e.getSource() == b5)
+	         t1.setText(t1.getText() + "4");
+	     else if(e.getSource() == b6)
+	         t1.setText(t1.getText() + "5");
+	     else if(e.getSource() == b7)
+	         t1.setText(t1.getText() + "6");
+	     else if(e.getSource() == b9)
+	         t1.setText(t1.getText() + "7");
+	     else if(e.getSource() == b10)
+	         t1.setText(t1.getText() + "8");
+	     else if(e.getSource() == b11)
+	         t1.setText(t1.getText() + "9");
+	     else if(e.getSource() == b14)
+	         t1.setText(t1.getText() + "0");
+	     else if(e.getSource() == b4) {
+	    	 String s = t1.getText();
+	         n = Integer.parseInt(s);
+	         t1.setText("");
+	         i = 1;
+	     }
+	     else if(e.getSource() == b8) {
+	         String s = t1.getText();
+	         n = Integer.parseInt(s);
+	         t1.setText("");
+	         i = 2;
+	     }
+	     else if(e.getSource() == b12) {
+	         String s=t1.getText();
+	         n=Integer.parseInt(s);
+	         t1.setText("");
+	         i=3;
+	     }
+	     else if(e.getSource()==b13) {
+	         String s=t1.getText();
+	         n=Integer.parseInt(s);
+	         t1.setText("");
+	         i=4;
+	     }
+	     else if(e.getSource() == b15) {
+	         calc(n ,i);
+	     }
+	     else {
+	    	 t1.setText("");
+	     }
+	}
+	
+	void calc(int t, int e) {
+		int res;
+		switch(e) {
+		case 1:
+			res = t + Integer.parseInt(t1.getText());
+			t1.setText("");
+			t1.setText(Integer.toString(res));
+			break;
+		case 2:
+			res=t-Integer.parseInt(t1.getText());
+			t1.setText("");
+			t1.setText(Integer.toString(res));
+			break;
+		case 3:
+			res=t*Integer.parseInt(t1.getText());
+			t1.setText("");
+			t1.setText(Integer.toString(res));
+			break;
+		case 4:
+			res=t/Integer.parseInt(t1.getText());
+			t1.setText("");
+			t1.setText(Integer.toString(res));
+			break;
+		default:
+			break;
+		}
 	}
 
 	public static void main(String[] args) {
